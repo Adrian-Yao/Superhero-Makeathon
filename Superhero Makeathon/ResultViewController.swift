0ae362cdeadac22430e25c86feef105eb08cdc96
Adrian-Yao <adrian.c.yao@gmail.com>
2017-08-01 09:40:27 -0400

    First commit

---
+++ Superhero Makeathon/ResultViewController.swift
@@ -0,0 +1,17 @@
+//
+//  resultViewcontroller.swift
+//  Superhero Makeathon
+//
+//  Created by adyao20 on 7/31/17.
+//  Copyright © 2017 adyao20. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class resultViewcontroller:UIViewController {
+    
+    
+    
+    
+}
---
+++ Superhero Makeathon/SurveyViewController.swift
@@ -0,0 +1,21 @@
+//
+//  surveyViewController.swift
+//  Superhero Makeathon
+//
+//  Created by adyao20 on 7/31/17.
+//  Copyright © 2017 adyao20. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+ class SurveyViewController:UIViewController {
+    
+    @IBOutlet weak var amountOfPeople: UILabel!
+    
+    @IBAction func stepper(_ sender: UIStepper) {
+        amountOfPeople.text = String(sender.value)
+    }
+}
+
